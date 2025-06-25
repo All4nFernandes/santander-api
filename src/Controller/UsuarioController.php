47690bb916853c9b799356c1d909ad810d97e70b
All4nfernandes <allandossantosfernandes@gmail.com>
2025-06-24 21:26:29 -0400

    criando regra para criar usuario

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -3,25 +3,76 @@
 namespace App\Controller;
 
 use App\Dto\UsuarioDto;
+use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route("/api")]
-final class UsuarioController extends AbstractController
+class UsuarioController extends AbstractController
 {
     #[Route('/usuarios', name: 'usuarios_criar', methods: ['POST'])]
-     public function Criar(
-        #[MapRequestPayload(acceptFormart:'json')]
-        UsuarioDto $usuarioDto
-        ):JsonResponse{
-            dd($usuarioDto);
-            return $this->json([
-                'messagem' => "Welcome"
-            ]);
+    public function criar(
+        #[MapRequestPayload(acceptFormat: 'json')]
+        UsuarioDto $usuarioDto,
+        UsuarioRepository $usuarioRepository,
 
+
+        EntityManagerInterface $entityManager
+
+    ): JsonResponse {
+
+        
+
+        $erros = [];
+
+
+
+        if (!($usuarioDto->getNome())) {
+            $erros[] = ["message" => "Nome é obrigatório!"];
         }
+        if (!($usuarioDto->getEmail())) {
+            $erros[] = ["message" => "Email é obrigatório!"];
+        }
+
+        if (!($usuarioDto->getTelefone())) {
+            $erros[] = ["message" => "Telefone é obrigatório!"];
+        }
+        if (!($usuarioDto->getCpf())) {
+            $erros[] = ["message" => "Cpf é obrigatório!"];
+        }
+        if (!($usuarioDto->getSenha())) {
+            $erros[] = ["message" => "Senha é obrigatório!"];
+        }
+
+        if (count($erros) > 0) {
+            return $this->json($erros, 422);
+        }
+
+        //valida se o cpf ja esta cadastrado
+        $usuarioExixstente = $usuarioRepository->findByCpf($usuarioDto->getCpf());
+
+        if($usuarioExixstente){
+            return $this->json([ 
+                "message" => "O CPF informado já está cadastrado"
+            ],409);
+        }
+
+        // converte o DTO em entidade usuário
+        $usuario = new Usuario();
+        $usuario->setCpf($usuarioDto->getCpf());
+        $usuario->setNome($usuarioDto->getNome());
+        $usuario->setEmail($usuarioDto->getEmail());
+        $usuario->setSenha($usuarioDto->getSenha());
+        $usuario->setTelefone($usuarioDto->getTelefone());
+
+        // criar o registro na tb usuario
+        $entityManager->persist($usuario);
+        $entityManager->flush();
+
+        return $this->json($usuario);
+    }
 }
-
-
